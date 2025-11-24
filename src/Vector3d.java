bb32526563c9570358d645bea997937b697f8011
Harman <hsingh4845@gmail.com>
2025-11-24 01:01:09 -0800

    Added helper functions to Vector3d,

--- src/Vector3d.java
+++ src/Vector3d.java
@@ -17,6 +17,19 @@ public record Vector3d(double x, double y, double z) {
         return new Vector3d(this.x * vector.x, this.y * vector.y, this.z * vector.z);
     }
 
+    public double distance(Vector3d point) {
+        Vector3d v = this.add(point.multiply(-1));
+        v = v.multiply(v);
+        double distance_squared = v.x + v.y + v.z;
+
+        return Math.sqrt(distance_squared);
+    }
+    public double distance_squared(Vector3d point) {
+        Vector3d v = this.add(point.multiply(-1));
+        v = v.multiply(v);
+        return v.x + v.y + v.z;
+    }
+
     @Override
     public String toString() {
         return "(" + x + ", " + y + ", " + z + ")";
