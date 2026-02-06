d423a134fd74b9e17c570c5f489e04b6446b8b72
Harman <hsingh4845@gmail.com>
2026-02-05 20:49:00 -0800

    -Additional aircraft info is displayed when selected. -Refined the selection square gui when aircraft is selected.

--- src/main/java/com/tcas_sim/util/transfer/AircraftDataTransfer.java
+++ src/main/java/com/tcas_sim/util/transfer/AircraftDataTransfer.java
@@ -2,5 +2,5 @@ package main.java.com.tcas_sim.util.transfer;
 
 import main.java.com.tcas_sim.util.math.Vector3d;
 
-public record AircraftDataTransfer(String callsign, Vector3d pos, boolean isTA, boolean isRA) {
+public record AircraftDataTransfer(String callsign, Vector3d pos, double bearing, boolean isTA, boolean isRA) {
 }
